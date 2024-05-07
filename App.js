8202dca33b48042eb2bb714fdb099a4c25ec9b91
yacine Hadrine <yacineyacine31212@gmail.com>
2024-05-07 21:33:36 +0100

    before configure eslint and prettier

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import {View, Text} from 'react-native';
 View;
 const App = ({}) => {
   return (
-    <View>
+    <View style={{flex:1, alignItems:'center', justifyContent:'center'}}>
       <Text>hello World</Text>
     </View>
   );
