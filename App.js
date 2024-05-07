59e38afeda5abbd6d7c8f42e1048855317aab06f
yacine Hadrine <yacineyacine31212@gmail.com>
2024-05-07 19:44:49 +0100

    remove app.tsx and replace it by app.js

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+View;
+const App = ({}) => {
+  return (
+    <View>
+      <Text>hello World</Text>
+    </View>
+  );
+};
+
+export default App;
