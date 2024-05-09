3c4138b0284503d484231902e59e37bd2632081e
yacine Hadrine <yacineyacine31212@gmail.com>
2024-05-09 21:03:46 +0100

    setup retaurent info

---
+++ src/features/restaurants/components/restaurent-info.component.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+const RestaurentInfo = () =>  {
+  return (
+    <Text>
+      restaurent Info
+    </Text>
+  );
+};
+
+export default RestaurentInfo;
