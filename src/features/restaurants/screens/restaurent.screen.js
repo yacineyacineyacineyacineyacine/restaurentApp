e9b1ccfdc2ffac8cb0bf4280524b8b04438d6895
yacine Hadrine <yacineyacine31212@gmail.com>
2024-05-09 20:55:24 +0100

    move App.js content to restaurent.screen.js

---
+++ src/features/restaurants/screens/restaurent.screen.js
@@ -0,0 +1,56 @@
+import * as React from 'react';
+import { SafeAreaView, View, Text , StyleSheet} from 'react-native';
+import { Searchbar } from 'react-native-paper';
+import Icon from 'react-native-vector-icons/FontAwesome5' ;
+
+const Restaurentscreen =  () =>  {
+  return (
+    <>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.search}>
+          <Searchbar 
+            placeholder='Search'
+            placeholderTextColor='#a9a9ac'
+            icon={() => <Icon name='search' size={20} color='#909090' />}
+            style={styles.searchBar}
+            inputStyle={styles.searchBarInputStyle}
+            elevation={10}
+           />
+        </View>
+        <View style={styles.list}>
+          <Text>List</Text>
+        </View>
+      </SafeAreaView>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      flexDirection: 'column',
+      // marginTop: StatusBar.currentHeight,
+    },
+    search: {
+      padding: 16,
+    },
+    searchBar:{
+      borderRadius: 5,
+      elvation: 5
+    },
+    searchBarInputStyle:{
+      color: '#000',
+      fontFamily: 'roboto',
+      fontSize: 15,
+      fontStyle: 'normal',
+      fontWeight: '100',
+      letterSpacing: .5
+    },
+    list: {
+      flex: 1,
+      padding: 16,
+      backgroundColor: 'blue',
+    },
+  });
+
+export default Restaurentscreen;
