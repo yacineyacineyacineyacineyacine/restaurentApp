5fe04ba5a8dc9a4b60e4e08477b093c11b37abbf
yacine Hadrine <yacineyacine31212@gmail.com>
2024-05-19 20:27:19 +0100

    add close and close temporarly and icon

--- src/features/restaurants/components/restaurent-info-card.component.js
+++ src/features/restaurants/components/restaurent-info-card.component.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components/native'
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, Image } from 'react-native';
 import { SvgXml } from 'react-native-svg';
 
 import { Card } from 'react-native-paper';
@@ -17,13 +17,19 @@ const RestaurentCardCover = styled(Card.Cover)`
 const RestaurentCardContent = styled(Card.Content)`
   flex-direction: column;
 `;
-const InfoContainer = styled.View`
- flex-direction: row;
- justify-content: space-between;
-`;
+const Section = styled.View`
+  flex-direction: row;
+  align-items: center;
+ `;
 const Rating = styled.View`
   flex-direction: row;
 `;
+const SectionEnd = styled.View`
+  flex: 1;
+  flex-direction: row;
+  justify-content: flex-end;
+  align-items: 'center';
+`
 const Title = styled.Text`
   padding: ${(props) => props.theme.space[1]};
   color: ${(props) =>  props.theme.colors.ui.primary};
@@ -34,20 +40,23 @@ const Adress = styled.Text`
   padding: ${(props) => props.theme.space[0]};
   color: ${(props) =>  props.theme.colors.ui.primary};
   font-family: ${(props) => props.theme.fonts.body};
-  font-size: ${(props) => props.theme.fontSizes.body}
+  font-size: ${(props) => props.theme.fontSizes.body};
 `;
+const Spacing = styled.View`
+  padding-left: ${(props) => props.theme.space[2]};
+`
 const RestaurentInfoCard = ({restaurent = {}}) =>  {
   
     const {
         name = 'Some restaurent',
-        icon,
+        icon = 'https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png',
         photos = [
             'https://media.istockphoto.com/id/486548089/photo/melting-cheese-in-burger.jpg?s=1024x1024&w=is&k=20&c=rB1OQiPFkU48kRYeMBk_t7G6CMJUc8FdUkUWMQYGnE4='
         ],
         address = '100 some random street',
         isOpenNow = true,
         rating = 4,
-        isClosedTemporarily,
+        isClosedTemporarily = true,
     } = restaurent;
 
     const ratingArray = Array.from(new Array(Math.floor(rating)));
@@ -57,17 +66,25 @@ const RestaurentInfoCard = ({restaurent = {}}) =>  {
      <RestaurentCardCover key={name} style={styles.cover}source={{uri: photos[0]}} />
      <RestaurentCardContent>
       <Title>{name}</Title>
-      <InfoContainer>
-      <Rating>
-        {ratingArray.map((item, index)=>{
-          return  <SvgXml key={index} xml={star} width={20} height={20}/>
-        })}
-      </Rating>
-      {isOpenNow? <SvgXml xml={open} width={20} height={20}/> : null}
-      </InfoContainer>
-     
+      <Section>
+        <Rating>
+          {ratingArray.map((item, index)=>{
+            return  <SvgXml key={index} xml={star} width={20} height={20}/>
+          })}
+        </Rating>
+        <SectionEnd>
+          {isClosedTemporarily && (
+          <Text varinat='label' style={{color: 'red'}}> 
+             CLOESED TEMPORARLY
+          </Text>)}
+          <Spacing />
+          {isOpenNow? <SvgXml xml={open} width={20} height={20}/> : null}
+          <Spacing />
+          <Image style={{width: 15, height: 15}} source={{uri:icon}}/>
+        </SectionEnd>
+      </Section>
       <Adress>{address}</Adress>
-      </RestaurentCardContent>
+     </RestaurentCardContent>
    </RestaurentCard>
   );
 };
