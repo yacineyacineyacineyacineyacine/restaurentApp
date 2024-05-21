e0eeca92d927e72dbd480fc5649ce7c9f85c04ae
yacine Hadrine <yacineyacine31212@gmail.com>
2024-05-21 15:44:25 +0100

    add icons to bottom-tabs

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { theme } from './src/infrastructure/theme/index';
 import Restaurentscreen from './src/features/restaurants/screens/restaurent.screen';
 import { SafeArea } from './src/components/utility/safe.area.component';
+import Ionicicons from 'react-native-vector-icons/Ionicons'
 
 
 const Tab = createBottomTabNavigator();
@@ -18,7 +19,30 @@ const App = ({}) => {
     <>
     <ThemeProvider theme={theme}>
       <NavigationContainer>
-        <Tab.Navigator>
+        <Tab.Navigator
+         screenOptions={({route})=>({
+          headerShown: false,
+          tabBarIcon: ({focused, color, size}) =>{
+            let iconName;
+            if(route.name === 'Restaurents'){
+               iconName = focused
+               ? 'restaurant'
+               : 'restaurant-outline';
+            }else if(route.name === 'Map') {
+              iconName = focused
+               ? 'map'
+               : 'map-outline';
+            }else if(route.name === 'Settings'){
+              iconName = focused
+               ? 'settings'
+               : 'settings-outline';
+            }
+            return <Ionicicons name={iconName} size={size} color={color}/>
+          },
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray'
+         })}
+        >
           <Tab.Screen name="Restaurents" component={Restaurentscreen} />
           <Tab.Screen name="Map" component={MapScreen} />
           <Tab.Screen name="Settings" component={SettingsScreen} />
