2354128a27f69d25a78148b3bb714e6f45b65ca8
yacine Hadrine <yacineyacine31212@gmail.com>
2024-05-26 21:23:54 +0100

    restructering navigation

--- App.js
+++ App.js
@@ -1,40 +1,11 @@
 import React from 'react';
-import { Settings, StatusBar, Text } from 'react-native';
+import { StatusBar } from 'react-native';
 import { ThemeProvider } from 'styled-components/native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Ionicicons from 'react-native-vector-icons/Ionicons';
-
 import { theme } from './src/infrastructure/theme/index';
-import Restaurentscreen from './src/features/restaurants/screens/restaurent.screen';
-import { SafeArea } from './src/components/utility/safe.area.component';
+
 import { RestaurantsContextProvider } from './src/services/restaurants/restaurents.context';
 import { LocationContextProvider } from './src/services/location/location.context';
-
-const Tab = createBottomTabNavigator();
-
-const TAB_ICONS = {
-  Restaurents: 'restaurant',
-  Map: 'map',
-  Settings: 'settings'
-}
-
-const tabBarIcon = (iconName) => ({color, size})  =>{
-  return <Ionicicons name={iconName} size={size} color={color}/>
-}
-
-const createScreenOptions = ({route}) =>{
-  const iconName = TAB_ICONS[route.name];
- return {
-  headerShown: false,
-  tabBarIcon: tabBarIcon(iconName),
-  tabBarActiveTintColor: 'tomato',
-  tabBarInactiveTintColor: 'gray'
-  }
-}
-
-const MapScreen = () => <SafeArea><Text>Settings screen</Text></SafeArea>
-const SettingsScreen = () => <SafeArea><Text>Settings screen</Text></SafeArea>
+import { Navigator } from './src/infrastructure/navigation/app.navigator';
 const App = ({}) => {
   
    return (
@@ -42,15 +13,7 @@ const App = ({}) => {
     <ThemeProvider theme={theme}>
       <LocationContextProvider>
         <RestaurantsContextProvider>
-          <NavigationContainer>
-            <Tab.Navigator
-            screenOptions = {createScreenOptions}         
-            >
-              <Tab.Screen name='Restaurents' component={Restaurentscreen} />
-              <Tab.Screen name='Map' component={MapScreen} />
-              <Tab.Screen name='Settings' component={SettingsScreen} />
-            </Tab.Navigator>
-          </NavigationContainer>
+          <Navigator/>
         </RestaurantsContextProvider>
       </LocationContextProvider>
     </ThemeProvider>
---
+++ src/infrastructure/navigation/app.navigator.js
@@ -0,0 +1,52 @@
+        
+import React from 'react';
+import { Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicicons from 'react-native-vector-icons/Ionicons';
+
+import Restaurentscreen from '../../features/restaurants/screens/restaurent.screen';
+import { SafeArea } from '../../components/utility/safe.area.component';
+
+const TAB_ICONS = {
+    Restaurents: 'restaurant',
+    Map: 'map',
+    Settings: 'settings'
+  }
+
+  const tabBarIcon = (iconName) => ({color, size})  =>{
+    return <Ionicicons name={iconName} size={size} color={color}/>
+  }
+  
+  const createScreenOptions = ({route}) =>{
+    const iconName = TAB_ICONS[route.name];
+   return {
+    headerShown: false,
+    tabBarIcon: tabBarIcon(iconName),
+    tabBarActiveTintColor: 'tomato',
+    tabBarInactiveTintColor: 'gray'
+    }
+  }
+  
+  const MapScreen = () => <SafeArea><Text>Settings screen</Text></SafeArea>
+  const SettingsScreen = () => <SafeArea><Text>Settings screen</Text></SafeArea>
+
+export const Navigator =() => {
+
+    const Tab = createBottomTabNavigator();
+
+    return (
+        <NavigationContainer>
+        <Tab.Navigator
+        screenOptions = {createScreenOptions}         
+        >
+          <Tab.Screen name='Restaurents' component={Restaurentscreen} />
+          <Tab.Screen name='Map' component={MapScreen} />
+          <Tab.Screen name='Settings' component={SettingsScreen} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    );
+};       
+
+       
+       
