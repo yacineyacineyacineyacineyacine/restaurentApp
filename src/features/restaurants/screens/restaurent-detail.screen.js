ee26afccd6cf7324b8a4d8d55e6a5f92f622c865
yacine Hadrine <yacineyacine31212@gmail.com>
2024-05-28 19:25:18 +0100

    add list items to list according

--- src/features/restaurants/screens/restaurent-detail.screen.js
+++ src/features/restaurants/screens/restaurent-detail.screen.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { ScrollView } from 'react-native';
 import { List } from 'react-native-paper'
 import { SafeArea  } from '../../../components/utility/safe.area.component';
 import styled from 'styled-components/native'
@@ -20,36 +21,48 @@ const RestaurentDetail  = ({navigation, route}) => {
     return (
       <SafeArea>
         <RestaurentInfoCard restaurant={restaurant}/>
-            <List.Accordion
-            title='Breakfast'
-            left={props => <List.Icon {...props} icon='bread-slice' />}
-            expanded={breakfastExpended}
-            onPress={() => setBreakfastExpended(prev => !prev)}
-            disableRipple>
-
+            <ScrollView>
+                <List.Accordion
+                title='Breakfast'
+                left={props => <List.Icon {...props} icon='bread-slice' />}
+                expanded={breakfastExpended}
+                onPress={() => setBreakfastExpended(prev => !prev)}>
+                    <List.Item title="Eggs Benedict" />
+                    <List.Item title="Classic Breakfast" />
+                </List.Accordion>
+                <List.Accordion
+                    title='Lunch'
+                    left={props => <List.Icon {...props} icon='hamburger' />}
+                    expanded={lunchExpended}
+                    onPress={() => setLunchExpended(prev => !prev)}>
+                        <List.Item title="Burger w/ Fries" />
+                        <List.Item title="Steak Sandwich" />
+                        <List.Item title="Mushroom Soup" />
+                </List.Accordion>
+                <List.Accordion
+                    title='Dinner'
+                    left={props => <List.Icon {...props} icon='food-variant' />}
+                    expanded={foodVariantExpended}
+                    onPress={() => setFoodVariantExpended(prev => !prev)}>
+                        <List.Item title="Spaghetti Bolognese" />
+                        <List.Item title="Veal Cutlet with Chicken Mushroom Rotini" />
+                        <List.Item title="Steak Frites" />
+                    
+                </List.Accordion>
+                <List.Accordion
+                    title='Drinks'
+                    left={props => <List.Icon {...props} icon='cup' />}
+                    expanded={drinksExpended}
+                    onPress={() => setDrinksExpended(prev => !prev)}>
+                        <List.Item title="Coffee" />
+                        <List.Item title="Tea" />
+                        <List.Item title="Modelo" />
+                        <List.Item title="Coke" />
+                        <List.Item title="Fanta" />
+                    
+                </List.Accordion>
+            </ScrollView>
             
-        </List.Accordion>
-        <List.Accordion
-            title='Lunch'
-            left={props => <List.Icon {...props} icon='hamburger' />}
-            expanded={lunchExpended}
-            onPress={() => setLunchExpended(prev => !prev)}>
-            
-        </List.Accordion>
-        <List.Accordion
-            title='Dinner'
-            left={props => <List.Icon {...props} icon='food-variant' />}
-            expanded={foodVariantExpended}
-            onPress={() => setFoodVariantExpended(prev => !prev)}>
-            
-        </List.Accordion>
-        <List.Accordion
-            title='Drinks'
-            left={props => <List.Icon {...props} icon='cup' />}
-            expanded={drinksExpended}
-            onPress={() => setDrinksExpended(prev => !prev)}>
-            
-        </List.Accordion>
       </SafeArea>
     );
 };
