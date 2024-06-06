427573ad1fdde5b311aeb148b0f897d3a26390a5
yacine Hadrine <yacineyacine31212@gmail.com>
2024-06-06 14:52:44 +0100

    add firebase and test it

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import 'react-native-gesture-handler';
-import React from 'react';
+import React, {useState, useEffect}from 'react';
 import { StatusBar } from 'react-native';
 import { ThemeProvider } from 'styled-components/native';
 import { theme } from './src/infrastructure/theme/index';
+import auth from '@react-native-firebase/auth'
 
 import { RestaurantsContextProvider } from './src/services/restaurants/restaurents.context';
 import { LocationContextProvider } from './src/services/location/location.context';
@@ -11,7 +12,34 @@ import { Navigation } from './src/infrastructure/navigation/';
 
 
 const App = ({}) => {
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState();
+  const [authenticated, setAuthenticated] = useState(false);
   
+  const onAuthStateChanged = (user) =>{
+    
+    setUser(user);
+    if(initializing) setInitializing(false);
+  }
+
+  useEffect(() => {
+    // const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    // return subscriber; // unsubscribe on unmount
+    setTimeout(() =>{
+      auth()
+      .signInWithEmailAndPassword('yacineyacine31212@gmail.com', 'yacine123')
+      .then((user) =>{
+        setAuthenticated(true)
+      }).catch(err => {
+        console.log(err);
+      })
+    }, 2000)
+
+  }, []);
+
+  // if (initializing) return null;
+  if (!authenticated) return null;
+
    return (
     <>
       <ThemeProvider theme={theme}>
